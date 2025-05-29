98b2f89fd29f608d6a7611f2b726718a2e4aa378
Aleksey Panin <lllrikitikilll@yandex.ru>
2025-05-29 17:40:32 +0300

    Добавлены файл запуска приожения

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ from app.api.router import router as router_api
 
 app = FastAPI()
 
-app.mount('/static', StaticFiles(directory='app/static'), 'static')
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(router_api)
