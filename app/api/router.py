7470d661cd882b4a875b2f30295955c4719bb219
Aleksey Panin <lllrikitikilll@yandex.ru>
2025-05-29 17:38:48 +0300

    Добавлены роуты get_main_page, get_weather_on_city

--- app/api/router.py
+++ app/api/router.py
@@ -1,21 +1,30 @@
-from fastapi import APIRouter, Request, HTTPException, Depends
-from fastapi.templating import Jinja2Templates
+from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
-router = APIRouter(prefix='', tags=['API'])
-templates = Jinja2Templates(directory='app/templates')
+from fastapi.templating import Jinja2Templates
+from httpx import AsyncClient
+
+from app.api.schemas import RequestSchema
+from app.api.weatherapi import weather_api
+
+router = APIRouter(prefix="", tags=["API"])
+templates = Jinja2Templates(directory="app/templates")
 
 
-@router.get('/')
+@router.get("/")
 async def get_main_page(request: Request):
-    return templates.TemplateResponse(name='index.html', context={'request': request})
+    return templates.TemplateResponse(
+        name="index.html", context={"request": request}
+    )
 
 
-@router.post('/weather/{city:str}')
-async def get_weather_on_city(city):
-    return JSONResponse({
-        "city": city,
-        "temperature": "Проверка",
-        "description": "Проверка",
-        "humidity": "Проверка",
-        "windSpeed": "Проверка"
-        })
+@router.post("/weather")
+async def get_weather_on_city(coords: RequestSchema):
+    async with AsyncClient() as client:
+        weather = await weather_api.get_basic_weather(
+            client, coords.lat, coords.lon
+        )
+    return JSONResponse(
+        {
+            "temperature": weather.temperature_2m,
+            "windSpeed": weather.wind_speed_10m}
+    )
