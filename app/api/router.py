dfee64c56930900aa2548a221c4ed884535c7abd
Aleksey Panin <lllrikitikilll@yandex.ru>
2025-05-30 13:45:53 +0300

    Добавлена запись кук и поиска  в БД функции get_weather_on_city

--- app/api/router.py
+++ app/api/router.py
@@ -3,9 +3,10 @@ from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from httpx import AsyncClient
 
-from app.api.dependencies import get_async_http_client
+from app.api.dependencies import get_async_http_client, get_session_id
 from app.api.schemas import RequestSchema
 from app.api.weatherapi import weather_api
+from app.dao.db_methods import add_search_log
 
 router = APIRouter(prefix="", tags=["API"])
 templates = Jinja2Templates(directory="app/templates")
@@ -18,9 +19,20 @@ async def get_main_page(request: Request):
 
 @router.post("/weather")
 async def get_weather_on_city(
-    coords: RequestSchema, client: AsyncClient=Depends(get_async_http_client)
-    ):
-    weather = await weather_api.get_basic_weather(client, coords.lat, coords.lon)
-    return JSONResponse(
+    search_request: RequestSchema,
+    client: AsyncClient = Depends(get_async_http_client),
+    session_id: str = Depends(get_session_id),
+):
+    weather = await weather_api.get_basic_weather(
+        client, search_request.lat, search_request.lon
+    )
+    response = JSONResponse(
         {"temperature": weather.temperature_2m, "windSpeed": weather.wind_speed_10m}
     )
+    await add_search_log(session_id=session_id, search_request=search_request.city)
+    response.set_cookie(
+        key="session_id",
+        value=session_id,
+        max_age=3600,
+    )
+    return response
