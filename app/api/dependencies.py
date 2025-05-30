59522c21c6c2bd0c22acaf70c76c5a7cbf539ba7
Aleksey Panin <lllrikitikilll@yandex.ru>
2025-05-30 13:45:04 +0300

    Добавена функция взятия session_id из кук

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,7 +1,15 @@
-from fastapi import Request
+import uuid
+
+from fastapi import Cookie
 from httpx import AsyncClient
 
 
 async def get_async_http_client():
     async with AsyncClient() as client:
         yield client
+
+
+def get_session_id(session_id: str = Cookie(None)):
+    if session_id is None:
+        session_id = str(uuid.uuid4())
+    return session_id
