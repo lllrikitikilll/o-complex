a9faac310c42b5161251919b7c02bcf4a19fd11d
Aleksey Panin <lllrikitikilll@yandex.ru>
2025-05-30 13:44:17 +0300

    Добавен декоратор connection

--- app/dao/database.py
+++ app/dao/database.py
@@ -1,7 +1,8 @@
+from sqlalchemy.ext.asyncio import (AsyncAttrs, async_sessionmaker,
+                                    create_async_engine)
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
-from app.core.settings import settings
 
+from app.core.settings import settings
 
 DATABASE_URL = settings.get_db_url()
 
@@ -9,5 +10,19 @@ engine = create_async_engine(url=DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
+def connection(method):
+    async def wrapper(*args, **kwargs):
+        async with async_session_maker() as session:
+            try:
+                return await method(*args, session=session, **kwargs)
+            except Exception as e:
+                await session.rollback()
+                raise e
+            finally:
+                await session.close()
+
+    return wrapper
+
+
 class Base(AsyncAttrs, DeclarativeBase):
-    __abstract__ = True 
+    __abstract__ = True
