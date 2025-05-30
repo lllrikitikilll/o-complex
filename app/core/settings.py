fcb16e7550f21ed45a3164237563115f9f662ac2
Aleksey Panin <lllrikitikilll@yandex.ru>
2025-05-30 10:18:56 +0300

    Добавены настройки

---
+++ app/core/settings.py
@@ -0,0 +1,21 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DATABASE: str
+    
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+    )
+
+    def get_db_url(self):
+        return (f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+                f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DATABASE}")
+
+        
+settings = Settings()
